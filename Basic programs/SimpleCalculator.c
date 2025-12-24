04ffb3eefb0611c42a8e26d5ed66593f414f7b68
vpaul57 <vpaul57@gmail.com>
2025-12-24 16:48:57 +1300

    new programs

---
+++ Basic programs/SimpleCalculator.c
@@ -0,0 +1,72 @@
+#include <stdio.h>
+
+float add(float a, float b) // Function to add two integers
+{
+    float sum = (a + b);
+    return sum;
+}
+
+float sub(float a,float b) // Function to subtract two integers
+{
+    float subtract = ( a -  b);
+    return subtract;
+}
+
+float mult(float a, float b) //Function to multiply two integers
+{
+    float multiply = ( a * b);
+    return multiply;
+}
+
+float divd( float a, float b) // Function to divide two numbers
+{
+    float divide = ( a / b);
+    return divide;
+}
+
+int main()
+{
+    float x,y;
+    char op;
+    printf("\n Enter the first number: ");
+    scanf(" %f", &x);
+    printf("\n Enter the second number: ");
+    scanf(" %f", &y);
+    printf ("\n Enter the operator :+,-,*,/ : ");
+    scanf(" %c", &op);
+
+    if (op == '+')
+    {
+        float sum = add(x,y);
+        printf("\n The sum of two numbers is %f", sum);
+        return 0;
+
+    }
+    else if (op == '-')
+    {
+        float subtract = sub(x,y);
+        printf("\n The difference of two numbers is %f", subtract);
+        return 0;
+    }
+    else if (op == '*')
+    {
+        float multiply = mult(x,y);
+        printf("\n The multiplication of two numbers is %f", multiply);
+        return 0;
+    }
+    else if (op == '/')
+    {
+        if (y == 0)
+        {
+            printf("\n Division by zero is not allowed.");
+            return 1;
+        }
+        else
+        {
+            float divide = divd(x,y);
+            printf("\n The division of two numbers is %f", divide);
+            return 0;
+        }
+    }
+}
+    
